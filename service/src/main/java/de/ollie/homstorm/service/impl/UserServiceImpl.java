4ece04865b55c9cab765ea00bcb8e56b94526353
ollie <lieshoff@gmx.de>
2020-03-29 17:07:33 +0200

    Made the password configurable.

--- service/src/main/java/de/ollie/homstorm/service/impl/UserServiceImpl.java
+++ service/src/main/java/de/ollie/homstorm/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package de.ollie.homstorm.service.impl;
 
 import javax.inject.Named;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import de.ollie.homstorm.service.UserService;
 
 /**
@@ -12,9 +14,12 @@ import de.ollie.homstorm.service.UserService;
 @Named
 public class UserServiceImpl implements UserService {
 
+	@Value("${homstorm.password}")
+	private String password;
+
 	@Override
 	public boolean isAccepted(String userName, String password) {
-		return userName.equals("user") && password.equals("ncc1701");
+		return userName.equals("user") && password.equals(this.password);
 	}
 
 }
